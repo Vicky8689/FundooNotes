0a3c1104ed08f6072feec703ee89624ed6d4fd6f
vicky <vickyy8689@gmail.com>
2024-05-29 15:37:27 +0530

    [vicky]: Add Registration Action method in Controller

---
+++ FundooNotes/Controllers/LoginRegistrationController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace FundooNotes.Controllers
+{
+    [ApiController]
+    [Route("fundoonotes")]
+    public class LoginRegistrationController : Controller
+    {
+          
+        [Route("Registration")]
+
+        public string  RegistrationController()
+        {
+            return "Controller added successfully";
+        }
+    }
+}
